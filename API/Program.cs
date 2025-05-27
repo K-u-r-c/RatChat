0d4f5b47f7b75954bcc10c761bfb60c6a9a3434a
K-U-R-C <jak.kurc@gmail.com>
2025-05-27 20:45:14 +0200

    Implement authorization policy in controllers and update logging level in development settings

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,13 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(opt =>
+{
+    var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+    opt.Filters.Add(new AuthorizeFilter(policy));
+});
 
 var app = builder.Build();
 
