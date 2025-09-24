424eabddc1303e778633e7744472aa2e27cf8309
Naxden <kilarjan05@gmail.com>
2025-09-24 18:48:06 +0200

    fix: Remove unnecessary blank lines in ChatRoomBanDto class

--- Application/ChatRooms/DTOs/ChatRoomBanDto.cs
+++ Application/ChatRooms/DTOs/ChatRoomBanDto.cs
@@ -6,8 +6,6 @@ public class ChatRoomBanDto
 {
     public required string UserId { get; set; }
     public UserProfileDto? User { get; set; } = null;
-
     public required string ChatRoomId { get; set; }
-
     public DateTime DateBanned { get; set; }
 }
